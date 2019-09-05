0160a3959feec2fda8591d0649f8648d9ca2b56b
joshcummingsdesign <joshcummingsdesign@gmail.com>
2019-09-05 13:22:30 -0700

    Remove es7 snippets from vscode extensions

--- scripts/vscodex.sh
+++ scripts/vscodex.sh
@@ -7,7 +7,6 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension editorconfig.editorconfig
-code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension mishkinf.goto-next-previous-member
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension esbenp.prettier-vscode
