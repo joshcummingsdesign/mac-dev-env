8c34de688deec99a8d8d2f6b7ea3de0ff64be565
Josh Cummings <joshcummingsdesign@gmail.com>
2020-10-22 17:05:21 -0700

    Add php extensions for vscode

--- scripts/vscodex.sh
+++ scripts/vscodex.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Install extensions
 code --install-extension patbenatar.advanced-new-file
 code --install-extension ginfuru.ginfuru-better-solarized-dark-theme
 code --install-extension wmaurer.change-case
@@ -9,6 +10,9 @@ code --install-extension ms-azuretools.vscode-docker
 code --install-extension editorconfig.editorconfig
 code --install-extension mishkinf.goto-next-previous-member
 code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension higoka.php-cs-fixer
+code --install-extension ikappas.phpcs
 code --install-extension esbenp.prettier-vscode
 code --install-extension vscodevim.vim
 code --install-extension ashhitch.vs-code-css-comments
