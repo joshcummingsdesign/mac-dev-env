59ff381abeaa1b486bc5fbffc3543bfc02e5fa7e
Josh Cummings <joshcummingsdesign@gmail.com>
2022-08-14 06:54:42 -0700

    add python to vscode

--- scripts/vscodex.sh
+++ scripts/vscodex.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 # Install extensions
-code --install-extension patbenatar.advanced-new-file
-code --install-extension ginfuru.ginfuru-better-solarized-dark-theme
-code --install-extension wmaurer.change-case
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension editorconfig.editorconfig
-code --install-extension mishkinf.goto-next-previous-member
-code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension ashhitch.vs-code-css-comments
+code --install-extension bajdzis.vscode-twig-pack
 code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension dunstontc.viml
+code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension ginfuru.ginfuru-better-solarized-dark-theme
 code --install-extension higoka.php-cs-fixer
 code --install-extension ikappas.phpcs
-code --install-extension esbenp.prettier-vscode
-code --install-extension bajdzis.vscode-twig-pack
-code --install-extension vscodevim.vim
-code --install-extension ashhitch.vs-code-css-comments
 code --install-extension jpoissonnier.vscode-styled-components
-code --install-extension dunstontc.viml
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension mishkinf.goto-next-previous-member
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension patbenatar.advanced-new-file
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vscodevim.vim
+code --install-extension wmaurer.change-case
