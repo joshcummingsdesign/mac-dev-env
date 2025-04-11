dd5168c015fd311b0073462322685336a743f4fb
Josh Cummings <joshcummingsdesign@gmail.com>
2025-04-11 14:37:42 -0700

    Change python formatter

--- scripts/vscodex.sh
+++ scripts/vscodex.sh
@@ -7,6 +7,7 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension mishkinf.goto-next-previous-member
+code --install-extension ms-python.black-formatter
 code --install-extension ms-python.python
 code --install-extension patbenatar.advanced-new-file
 code --install-extension streetsidesoftware.code-spell-checker
