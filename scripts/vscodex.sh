41751c77dd2cb2deb78325b1fb3fc08e508eed03
Josh Cummings <joshcummingsdesign@gmail.com>
2025-04-14 11:11:39 -0700

    Add isort

--- scripts/vscodex.sh
+++ scripts/vscodex.sh
@@ -8,6 +8,7 @@ code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension mishkinf.goto-next-previous-member
 code --install-extension ms-python.black-formatter
+code --install-extension ms-python.isort
 code --install-extension ms-python.python
 code --install-extension patbenatar.advanced-new-file
 code --install-extension streetsidesoftware.code-spell-checker
